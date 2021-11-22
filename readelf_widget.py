982edc78c3f8f0f58a5b9b463d09f7f544169ace
Nick Thijssen <nickthijssen1994@gmail.com>
2021-11-22 15:47:57 +0100

    Update readelf_widget.py

--- readelf_widget.py
+++ readelf_widget.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 
 from PySide6.QtWidgets import QWidget, QTextEdit, QVBoxLayout
 
@@ -14,9 +15,19 @@ class ReadElfWidget(QWidget):
         self.layout.addWidget(self._fileContent)
 
     def set_file(self, file):
-        stream = os.popen('readelf -h ' + file)
-        output = stream.read()
-        self._fileContent.setText(output)
+        ssh = subprocess.Popen(["readelf", "-h", file],
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE,
+                               universal_newlines=True,
+                               bufsize=0)
+        for line in ssh.stdout:
+            self._fileContent.append(line.strip())
+        # try:
+        #     stream = os.popen('readelf -h' + file)
+        #     output = stream.read()
+        #     self._fileContent.setText(output)
+        # except:
+        #     self._fileContent.setText('Could Not Read File Content')
 
     def clear_file(self):
         self._fileContent.clear()
